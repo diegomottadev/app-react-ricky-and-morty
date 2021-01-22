797413675d6606a510b8a69509f2da2ac6754c75
diego <diegomotta18@gmail.com>
2021-01-22 15:27:57 -0300

    ADD: conectando redux con componentes react

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,37 +2,17 @@ import React, { useState, useEffect } from 'react'
 import Card from '../card/Card'
 import styles from './home.module.css'
 import axios from 'axios'
+import { connect } from 'react-redux'
 
 let URL = "https://rickandmortyapi.com/api"
 
-export default function Home() {
-
-    let [chars, setChars] = useState([])
-
-    useEffect(() => {
-        getCharacters()
-    }, [])
-
-    function nextChar() {
-        chars.shift()
-        if (!chars.length) {
-            //get more characters
-        }
-        setChars([...chars])
-    }
+ function Home() {
 
     function renderCharacter() {
-        let char = chars[0]
+        //let char = chars[0]
         return (
-            <Card leftClick={nextChar} {...char} />
+            <Card />
         )
-    }
-
-    function getCharacters() {
-        return axios.get(`${URL}/character`)
-            .then(res => {
-                setChars(res.data.results)
-            })
     }
 
     return (
@@ -44,3 +24,5 @@ export default function Home() {
         </div>
     )
 }
+
+export default connect()(Home)
