21157d7e9f53c00192d85ed73f5fa9d1a8573d74
diego <diegomotta18@gmail.com>
2021-01-22 12:33:40 -0300

    ADD:fix ERR_INVALID_ARG_TYPE

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,18 @@
-
 // este archivo sirve para conectar React con Redux
-
-import React from 'react';
 import {createStore,combineReducers,compose,applyMiddleware} from 'redux';
 import userReducer from './userDuck';
 import thunk from 'redux-thunk';
+
 let rootReducer = combineReducers({
-    user:userReducer
+    user: userReducer
 })
 //Ver herramientas de desarrollador
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 //applyMiddleware // usar la herramienta soportar promesas
 export default function generateStore(){
-    let store = createStore(rootReducer,
-                            composeEnhancers(applyMiddleware(thunk)))
-        return store;
+    let store = createStore(
+        rootReducer,
+        composeEnhancers(applyMiddleware(thunk))
+    )
+    return store;
 }
--- src/redux/userDuck.js
+++ src/redux/userDuck.js
@@ -6,7 +6,6 @@ let LOGIN = "LOGIN";
 
 export default  function reducer (state= initialData,action){
     switch (action.type){
-        case LOGIN:
         default:
             return state;
     }
